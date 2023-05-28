c3f3ab3648c0dc7fafb534223fee435542ed7df8
J7 <j7.dev.gg@outlook.com>
2023-05-28 17:55:08 +0800

    feat: add react-query provider

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { ChakraProvider } from "@chakra-ui/react";
 import App from "./App";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./index.css";
+
+// Create a client
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
