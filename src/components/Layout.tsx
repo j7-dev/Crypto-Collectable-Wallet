389090ab1f6a55512f45760b11d37361f5956bf8
J7 <j7.dev.gg@outlook.com>
2023-05-28 23:30:02 +0800

    feat: refactor router

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,13 +1,13 @@
-export const Layout: React.FC<{ children: React.ReactNode }> = ({
-  children,
-}) => {
+import { Link, Outlet } from "react-router-dom";
+
+export const Layout = () => {
   return (
     <div className="bg-gray-100">
       <div className="w-full bg-teal-500 text-center mb-4 py-2 fixed top-0 left-0 font-bold text-white z-50">
-        Crypto Collectable Wallet
+        <Link to="/">Crypto Collectable Wallet</Link>
       </div>
       <div className="mx-auto xl:w-[1080px] px-4 xl:px-8 pt-16 min-h-screen overflow-hidden">
-        {children}
+        <Outlet />
       </div>
     </div>
   );
--- src/router.tsx
+++ src/router.tsx
@@ -1,13 +1,21 @@
 import { createBrowserRouter } from "react-router-dom";
 import { List, Detail } from "@/pages";
+import { Layout, ErrorPage } from "@/components";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <List />,
-  },
-  {
-    path: "/detail/:id",
-    element: <Detail />,
+    element: <Layout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "",
+        element: <List />,
+      },
+      {
+        path: "/detail/:id",
+        element: <Detail />,
+      },
+    ],
   },
 ]);
