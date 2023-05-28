aec53b506b7e870a80fba3023592fec2a9837625
J7 <j7.dev.gg@outlook.com>
2023-05-28 17:15:30 +0800

    feat: add basic List Layout

--- src/pages/list/index.tsx
+++ src/pages/list/index.tsx
@@ -1,7 +1,16 @@
-import React from "react";
+import { SimpleGrid, Box } from "@chakra-ui/react";
+import AssetCard from "./AssetCard";
 
 const index = () => {
-  return <div>List</div>;
+  return (
+    <div className="mx-auto w-[1080px] p-8 bg-gray-200">
+      <SimpleGrid columns={2} spacing="2rem">
+        {new Array(10).fill(0).map((_, i) => (
+          <AssetCard key={i} />
+        ))}
+      </SimpleGrid>
+    </div>
+  );
 };
 
 export default index;
