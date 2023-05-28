b5b430ddcecfa85fa85dd07582c1eda23c38966f
J7 <j7.dev.gg@outlook.com>
2023-05-28 18:51:34 +0800

    feat: add Link to detail page

--- src/components/AssetCard/index.tsx
+++ src/components/AssetCard/index.tsx
@@ -1,8 +1,10 @@
 import { Card, CardBody, Image, Stack, Heading, Text } from "@chakra-ui/react";
 import { TAssetCard } from "./types";
 import noimage from "@/assets/noimage.png";
+import { Link } from "react-router-dom";
 
 export * from "./types";
+
 export const AssetCard: React.FC<{ asset: TAssetCard }> = ({ asset }) => {
   const imageUrl = asset?.image_url || noimage;
   const name = asset?.name || "No name";
@@ -11,28 +13,30 @@ export const AssetCard: React.FC<{ asset: TAssetCard }> = ({ asset }) => {
   const collectionImageUrl = asset?.collection?.image_url || "";
 
   return (
-    <Card maxW="xl">
-      <CardBody>
-        <Image src={imageUrl} alt={name} borderRadius="lg" />
-        <Stack mt="6" spacing="3">
-          <Heading size="md">{name}</Heading>
-          <Text>{description}</Text>
-          {!!collectionName && (
-            <div className="flex bg-blue-400 items-center text-white text-xs px-3 py-1 rounded-xl w-fit">
-              {!!collectionImageUrl && (
-                <Image
-                  className="h-4 w-4 mr-2"
-                  src={collectionImageUrl}
-                  alt={name}
-                  borderRadius="full"
-                />
-              )}
-              {collectionName}
-            </div>
-          )}
-        </Stack>
-      </CardBody>
-    </Card>
+    <Link to={`/detail/${asset?.id}`}>
+      <Card maxW="xl">
+        <CardBody>
+          <Image src={imageUrl} alt={name} borderRadius="lg" />
+          <Stack mt="6" spacing="3">
+            <Heading size="md">{name}</Heading>
+            <Text>{description}</Text>
+            {!!collectionName && (
+              <div className="flex bg-blue-400 items-center text-white text-xs px-3 py-1 rounded-xl w-fit">
+                {!!collectionImageUrl && (
+                  <Image
+                    className="h-4 w-4 mr-2"
+                    src={collectionImageUrl}
+                    alt={name}
+                    borderRadius="full"
+                  />
+                )}
+                {collectionName}
+              </div>
+            )}
+          </Stack>
+        </CardBody>
+      </Card>
+    </Link>
   );
 };
 
