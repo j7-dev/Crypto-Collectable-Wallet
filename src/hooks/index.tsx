6594222d3e30411ecb41d0927c91fd33174d6663
J7 <j7.dev.gg@outlook.com>
2023-05-28 21:50:13 +0800

    feat: add useInfinityScroll custom hook

--- src/hooks/index.tsx
+++ src/hooks/index.tsx
@@ -1,3 +1,4 @@
 export * from "./useMany";
 export * from "./useOne";
 export * from "./useReadMore";
+export * from "./useInfinityScroll";
---
+++ src/hooks/useInfinityScroll.tsx
@@ -0,0 +1,65 @@
+import { useState, useEffect } from "react";
+import { debounce } from "lodash-es";
+import { TQuerryParams } from "@/types";
+
+export function useInfinityScroll<T>({
+  setQueryParams,
+  setEnabled,
+  isLoading,
+  list,
+  limit = 20,
+}: {
+  setQueryParams: React.Dispatch<React.SetStateAction<TQuerryParams>>;
+  setEnabled: React.Dispatch<React.SetStateAction<boolean>>;
+  isLoading: boolean;
+  list: T[];
+  limit?: number;
+}) {
+  const [copyList, setCopyList] = useState<T[]>([]);
+  const [isBottom, setIsBottom] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = debounce(() => {
+      const windowHeight = window.innerHeight;
+      const documentHeight = document.documentElement.scrollHeight;
+      const scrollTop = window.scrollY || document.documentElement.scrollTop;
+      const distanceToBottom = documentHeight - (scrollTop + windowHeight);
+      // start fetching when the distance to bottom is over threshold
+      const threshold = 0.5 * windowHeight;
+
+      if (distanceToBottom <= threshold) {
+        setQueryParams((pre) => {
+          const offset = parseInt(pre?.offset || "0", 10);
+          const limit = parseInt(pre?.limit || "0", 10);
+
+          return {
+            ...pre,
+            offset: (offset + 1 * limit).toString(),
+          };
+        });
+        setIsBottom(true);
+      } else {
+        setIsBottom(false);
+      }
+    }, 500);
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (!isLoading && list.length > 0) {
+      setCopyList((pre) => [...pre, ...list]);
+    }
+    if (!isLoading && list.length === 0) {
+      setEnabled(false);
+    }
+  }, [list.length, isLoading]);
+
+  return {
+    copyList,
+    isBottom,
+  };
+}
