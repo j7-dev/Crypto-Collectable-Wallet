d0b0ad8a6164e1678f3007171441f92522724750
J7 <j7.dev.gg@outlook.com>
2023-05-28 17:29:18 +0800

    feat: modify Layout css

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,8 +1,10 @@
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
-    <div>
-      <div className="w-full bg-yellow-500 text-center">This is Layout</div>
-      {children}
+    <div className="bg-gray-100">
+      <div className="w-full bg-yellow-500 text-center mb-4 py-2">
+        This is Layout
+      </div>
+      <div className="mx-auto xl:w-[1080px] px-4 xl:px-8">{children}</div>
     </div>
   );
 };
--- src/pages/list/index.tsx
+++ src/pages/list/index.tsx
@@ -3,7 +3,7 @@ import AssetCard from "./AssetCard";
 
 const index = () => {
   return (
-    <div className="mx-auto w-[1080px] p-8 bg-gray-200">
+    <div className="">
       <SimpleGrid columns={2} spacing="2rem">
         {new Array(10).fill(0).map((_, i) => (
           <AssetCard key={i} />
