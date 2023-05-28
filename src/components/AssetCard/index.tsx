15e202db03b18dff27d400e26615cf81122df3aa
J7 <j7.dev.gg@outlook.com>
2023-05-28 20:51:31 +0800

    feat: detail page api integration

--- src/components/AssetCard/index.tsx
+++ src/components/AssetCard/index.tsx
@@ -11,15 +11,28 @@ export const AssetCard: React.FC<{ asset: TAssetCard }> = ({ asset }) => {
   const description = asset?.description || "";
   const collectionName = asset?.collection?.name || "";
   const collectionImageUrl = asset?.collection?.image_url || "";
+  const tokenId = asset?.token_id || "";
+  const contractAddress = asset?.asset_contract?.address || "";
 
   return (
-    <Link to={`/detail/${asset?.id}`}>
+    <Link
+      to={`/detail/${asset?.id}`}
+      state={{
+        tokenId,
+        contractAddress,
+      }}
+    >
       <Card maxW="xl">
         <CardBody>
-          <Image src={imageUrl} alt={name} borderRadius="lg" />
+          <Image
+            src={imageUrl}
+            alt={name}
+            className="w-full aspect-square object-contain"
+            borderRadius="lg"
+          />
           <Stack mt="6" spacing="3">
             <Heading size="md">{name}</Heading>
-            <Text>{description}</Text>
+            <Text noOfLines={2}>{description}</Text>
             {!!collectionName && (
               <div className="flex bg-blue-400 items-center text-white text-xs px-3 py-1 rounded-xl w-fit">
                 {!!collectionImageUrl && (
--- src/pages/detail/index.tsx
+++ src/pages/detail/index.tsx
@@ -1,9 +1,25 @@
 import { Image, Heading, Button } from "@chakra-ui/react";
 import { ChevronLeftIcon } from "@chakra-ui/icons";
-import { Link } from "react-router-dom";
-import { useReadMore } from "@/hooks";
+import { Link, useParams, useLocation } from "react-router-dom";
+import { useReadMore, useOne } from "@/hooks";
+import noimage from "@/assets/noimage.png";
 
 const index = () => {
+  const { id } = useParams<{ id: string }>();
+  const { state } = useLocation();
+  const tokenId: string = state?.tokenId;
+  const contractAddress: string = state?.contractAddress;
+
+  const result = useOne({
+    resource: "asset",
+    pathParams: [contractAddress, tokenId],
+  });
+  const asset = result?.data?.data;
+  const name = asset?.name || "";
+  const permalink = asset?.permalink || "";
+  const imgUrl = asset?.image_url || noimage;
+  console.log("🚀 ~ file: index.tsx:8 ~ index ~ result:", asset);
+
   const { renderText } = useReadMore({
     originText:
       "Excepteur ex sunt dolore Lorem id ad sit qui excepteur id aliqua. Est pariatur duis sit ad elit id ipsum. Culpa in amet consequat labore nostrud laborum voluptate nisi. Enim sit aliquip officia esse exercitation officia esse aliquip. Eiusmod ad Lorem consectetur sunt dolor eiusmod aliquip incididunt.Non officia eu enim consequat. Exercitation voluptate consequat aliquip consectetur culpa sunt anim amet ea incididunt. Elit ipsum dolor sit fugiat labore.Tempor dolore aliquip adipisicing labore mollit laborum dolore dolore pariatur nisi enim nisi mollit. Reprehenderit incididunt velit labore sunt ea labore adipisicing do nostrud non quis eu. Reprehenderit culpa exercitation veniam sit excepteur officia mollit dolor ea. Qui ex dolore nostrud magna est aliquip adipisicing incididunt veniam tempor. Minim fugiat minim aliqua pariatur excepteur aute et veniam velit pariatur do ipsum. Aliquip labore id aliquip sit cillum labore aute consectetur. Minim et nulla consectetur consectetur aute pariatur proident amet irure.Cupidatat fugiat cillum officia esse exercitation voluptate est elit qui sint velit. Occaecat qui fugiat excepteur commodo in in quis consectetur eu. Velit eiusmod aliqua consequat ea.",
@@ -14,24 +30,30 @@ const index = () => {
         <Link to="/" className="absolute left-4">
           <ChevronLeftIcon w={8} h={8} color="gray.700" />
         </Link>
-        name
+        {name}
       </div>
       <Image
-        src="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-        alt="Green double couch with wooden legs"
+        src={imgUrl}
+        alt={name}
+        className="w-full aspect-square object-contain"
       />
       <div className="p-8">
         <Heading as="h1" size="xl">
-          Basic text writing, including headings, body text, lists, and more.
+          {name}
         </Heading>
+        <p className="text-xs text-gray-400">#{id}</p>
 
         <div className="mt-8 text-justify">{renderText()}</div>
       </div>
-      <div className="fixed bottom-4 w-full px-4">
-        <Button colorScheme="blue" className="w-full">
-          Permalink
-        </Button>
-      </div>
+      {!!permalink && (
+        <div className="fixed bottom-0 bg-white py-4 w-full px-4">
+          <a href={permalink} target="_blank">
+            <Button colorScheme="blue" className="w-full">
+              Permalink
+            </Button>
+          </a>
+        </div>
+      )}
     </div>
   );
 };
