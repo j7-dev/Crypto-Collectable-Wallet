b014f73a7ca1d83efa7b3d27891f1ed447d1c64b
J7 <j7.dev.gg@outlook.com>
2023-05-28 18:26:13 +0800

    feat: get assets and put into AssetCard

--- src/pages/list/index.tsx
+++ src/pages/list/index.tsx
@@ -1,12 +1,24 @@
-import { SimpleGrid, Box } from "@chakra-ui/react";
-import AssetCard from "./AssetCard";
+import { SimpleGrid } from "@chakra-ui/react";
+import { AssetCard, TAssetCard } from "@/components";
+import { useMany } from "@/hooks";
+import { owner } from "@/utils";
 
 const index = () => {
+  const result = useMany({
+    resource: "asset",
+    args: {
+      owner,
+      limit: "20",
+      offset: "0",
+    },
+  });
+  const assets = result?.data?.data?.assets || [];
+
   return (
     <div className="">
       <SimpleGrid columns={2} spacing="2rem">
-        {new Array(10).fill(0).map((_, i) => (
-          <AssetCard key={i} />
+        {assets.map((asset: TAssetCard) => (
+          <AssetCard key={asset?.id} asset={asset} />
         ))}
       </SimpleGrid>
     </div>
