79087cbb7f4a4a3af5e64e9634f458bebc8965d8
J7 <j7.dev.gg@outlook.com>
2023-05-28 18:20:40 +0800

    feat: move AssetCard to components, and add type

---
+++ src/components/AssetCard/types.ts
@@ -0,0 +1,108 @@
+export type TAssetCard = {
+  id: number;
+  token_id: string;
+  num_sales: number;
+  background_color: string | null;
+  image_url: string;
+  image_preview_url: string;
+  image_thumbnail_url: string;
+  image_original_url: string;
+  animation_url: string | null;
+  animation_original_url: string | null;
+  name: string;
+  description: string;
+  external_link: string | null;
+  asset_contract: {
+    address: string;
+    asset_contract_type: string;
+    chain_identifier: string;
+    created_date: string;
+    name: string;
+    nft_version: string;
+    opensea_version: string | null;
+    owner: number;
+    schema_name: string;
+    symbol: string;
+    total_supply: string;
+    description: string;
+    external_link: string;
+    image_url: string;
+    default_to_fiat: boolean;
+    dev_buyer_fee_basis_points: number;
+    dev_seller_fee_basis_points: number;
+    only_proxied_transfers: boolean;
+    opensea_buyer_fee_basis_points: number;
+    opensea_seller_fee_basis_points: number;
+    buyer_fee_basis_points: number;
+    seller_fee_basis_points: number;
+    payout_address: string | null;
+  };
+  permalink: string;
+  collection: {
+    banner_image_url: string;
+    chat_url: string | null;
+    created_date: string;
+    default_to_fiat: boolean;
+    description: string;
+    dev_buyer_fee_basis_points: string;
+    dev_seller_fee_basis_points: string;
+    discord_url: string | null;
+    display_data: {
+      card_display_style: string;
+      images: string | null;
+    };
+    external_url: string;
+    featured: boolean;
+    featured_image_url: string;
+    hidden: boolean;
+    safelist_request_status: string;
+    image_url: string;
+    is_subject_to_whitelist: boolean;
+    large_image_url: string;
+    medium_username: string | null;
+    name: string;
+    only_proxied_transfers: boolean;
+    opensea_buyer_fee_basis_points: string;
+    opensea_seller_fee_basis_points: number;
+    payout_address: string | null;
+    require_email: boolean;
+    short_description: string | null;
+    slug: string;
+    telegram_url: string | null;
+    twitter_username: string | null;
+    instagram_username: string | null;
+    wiki_url: string | null;
+    is_nsfw: boolean;
+    fees: {
+      seller_fees: {
+        [key: string]: any;
+      };
+      opensea_fees: {
+        "0x0000a26b00c1f0df003000390027140000faa719": number;
+      };
+    };
+    is_rarity_enabled: boolean;
+    is_creator_fees_enforced: boolean;
+  };
+  decimals: string | null;
+  token_metadata: string;
+  is_nsfw: boolean;
+  owner: string | null;
+  seaport_sell_orders: string | null;
+  creator: {
+    user: {
+      username: string | null;
+    };
+    address: string;
+    config: string;
+    profile_img_url: string;
+  };
+  traits: Array<any>;
+  last_sale: string | null;
+  top_bid: string | null;
+  listing_date: string | null;
+  supports_wyvern: boolean;
+  rarity_data: string | null;
+  transfer_fee: string | null;
+  transfer_fee_payment_token: string | null;
+};
