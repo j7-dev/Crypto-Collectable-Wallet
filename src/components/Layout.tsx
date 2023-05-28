70b4b5fe7d3ff86b2e792366ea7f0192c2c8da00
J7 <j7.dev.gg@outlook.com>
2023-05-28 22:47:56 +0800

    feat: modify header css class to fixed

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,7 +3,7 @@ export const Layout: React.FC<{ children: React.ReactNode }> = ({
 }) => {
   return (
     <div className="bg-gray-100">
-      <div className="w-full bg-teal-500 text-center mb-4 py-2 fixed absolute top-0 left-0 font-bold text-white">
+      <div className="w-full bg-teal-500 text-center mb-4 py-2 fixed top-0 left-0 font-bold text-white z-50">
         Crypto Collectable Wallet
       </div>
       <div className="mx-auto xl:w-[1080px] px-4 xl:px-8 pt-16 min-h-screen overflow-hidden">
