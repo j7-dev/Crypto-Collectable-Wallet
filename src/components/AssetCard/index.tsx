38e4223efef2d3c393bf21b01c8087847aae6a5f
J7 <j7.dev.gg@outlook.com>
2023-05-28 18:35:40 +0800

    feat: add default image

--- src/components/AssetCard/index.tsx
+++ src/components/AssetCard/index.tsx
@@ -11,14 +11,17 @@ import {
   Button,
 } from "@chakra-ui/react";
 import { TAssetCard } from "./types";
+import noimage from "@/assets/noimage.png";
 
 export * from "./types";
 export const AssetCard: React.FC<{ asset: TAssetCard }> = ({ asset }) => {
+  const image_url = asset?.image_url || noimage;
+
   return (
     <Card maxW="xl">
       <CardBody>
         <Image
-          src="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+          src={image_url}
           alt="Green double couch with wooden legs"
           borderRadius="lg"
         />
