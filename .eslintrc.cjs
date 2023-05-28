1ea59b28be24322d58fd1d80dfe0e61cd512c15d
J7 <j7.dev.gg@outlook.com>
2023-05-28 23:29:25 +0800

    feat: add eslint rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,7 +10,16 @@ module.exports = {
   parserOptions: { ecmaVersion: "latest", sourceType: "module" },
   plugins: ["react-refresh"],
   rules: {
-    "react-refresh/only-export-components": "warn",
+    "react-refresh/only-export-components": "off",
     "react-hooks/rules-of-hooks": "off",
+    "no-console": ["warn"],
+    "no-unused-vars": [
+      "warn",
+      {
+        argsIgnorePattern: "^_",
+        varsIgnorePattern: "^_",
+      },
+    ],
+    "no-shadow": "error",
   },
 };
