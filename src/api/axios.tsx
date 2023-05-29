ec698eb5072587fd428a7350e9e321e381f2e530
J7 <j7.dev.gg@outlook.com>
2023-05-29 21:07:36 +0800

    feat: add default action when API success or error

--- src/api/axios.tsx
+++ src/api/axios.tsx
@@ -6,4 +6,15 @@ const instance: AxiosInstance = axios.create({
   timeout: parseInt(apiTimeout, 10),
 });
 
+instance.interceptors.response.use(
+  async function (response) {
+    console.log("API success", response);
+    return Promise.resolve(response);
+  },
+  async function (error) {
+    console.log("API error", error);
+    return Promise.reject(error);
+  }
+);
+
 export default instance;
